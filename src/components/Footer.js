e0508630b76784fbe8dd0dea7a13fd75744dddb0
Salekin Imran <abir045@gmail.com>
2022-09-14 15:18:54 +0600

    updated footer in mb and tab layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,25 +14,26 @@ import instagramHover from "../assets/shared/desktop/instagramHover.svg";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col py-5 md:py-[15%] xl:py-10  bg-black  justify-between">
+    <div className="flex flex-col py-5  md:py-[5%] xl:py-5  bg-black  justify-between">
       {/* tablet */}
-      <div className="flex flex-col space-y-8 md:space-y-5 ">
-        <div className="hidden my-7 md:flex md:justify-between md:mx-[5%]">
+
+      <div className="flex flex-col space-y-8 md:space-y-10 xl:space-y-3 ">
+        <div className="hidden my-7 md:mt-5 md:mb-5 md:flex md:justify-between md:mx-[8%]">
           {/* logo */}
           <img
-            className="flex text-white w-[15%] h-[5%] md:w-[170px]"
+            className="flex text-white w-[15%] h-[5%]  md:w-[170px] md:-ml-5"
             src={logo}
           />
 
           {/* links */}
-          <ul className="hidden text-xs text-white font-bold uppercase tracking-[2px] md:mt-10 lg:mt-0 md:ml-[-62%] lg:ml-5  md:flex md:space-x-4 lg:space-x-0 xl:space-y-5  lg:flex lg:flex-col">
+          <ul className="hidden text-xs text-white font-bold uppercase tracking-[2px] md:mt-10 xl:mt-0 md:ml-[-68%] xl:ml-5  md:flex md:space-x-4 lg:space-x-0 xl:space-y-3  xl:flex xl:flex-col">
             <li className="hover:opacity-30">home</li>
             <li className="hover:opacity-30">stories</li>
             <li className="hover:opacity-30">features</li>
             <li className="hover:opacity-30">pricing</li>
           </ul>
           {/* button */}
-          <div className="flex space-x-3">
+          <div className="flex space-x-3  xl:space-x-5 xl:mr-[6%]">
             <button className="flex text-xs font-bold uppercase text-white tracking-[2px] hover:underline">
               get an invite
             </button>
@@ -40,7 +41,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="hidden  space-x-4  pb-[15%] xl:pb-5 md:mx-[5%] md:justify-between md:flex ">
+        <div className="hidden  space-x-3  md:w-[85%] pb-[5%] xl:pb-5 md:mx-[5%]  md:justify-between md:flex xl:w-[80%] xl:flex xl:ml-[7%]">
           {/*social icons container */}
           <div className="flex space-x-3">
             {/* fb logo */}
@@ -87,7 +88,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <p className="flex opacity-50 text-white text-[15px]">
+          <p className="flex opacity-50 text-white text-[15px] ">
             Copyright 2019. All Rights Reserved
           </p>
         </div>
@@ -98,7 +99,7 @@ const Footer = () => {
         />
 
         {/* icon container */}
-        <div className="flex  space-x-4 justify-center pb-[5%] md:pb-[5%]  md:hidden">
+        <div className="flex  space-x-4 justify-center pb-[5%]  md:hidden">
           {/* fb logo */}
           <div className="flex relative">
             <img className="absolute top-0  hover:opacity-0" src={Facebook} />
@@ -154,7 +155,7 @@ const Footer = () => {
           <img className="flex" src={arrow} alt="arrow" />
         </div>
 
-        <p className="flex opacity-50 justify-center pb-[10%] text-white text-[15px] md:hidden">
+        <p className="flex opacity-50 justify-center  pb-[10%]  text-white text-[15px] md:hidden">
           Copyright 2019. All Rights Reserved
         </p>
       </div>
